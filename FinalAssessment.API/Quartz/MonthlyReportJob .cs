7c5b4d265d62507534e806ac9ab9c55000e0ca6b
dogukanbey <dogukandiragan@hotmail.com>
2022-07-16 23:37:53 +0300

    Something about Quartz

---
+++ FinalAssessment.API/Quartz/MonthlyReportJob .cs
@@ -0,0 +1,23 @@
+﻿using Quartz;
+using FinalAssessment.Shared;
+
+namespace FinalAssessment.API.Quartz
+{
+    public class MonthlyReportJob : IJob
+    { 
+        EmailSender es = new EmailSender();
+        List<string> receiverlist = new List<string>(new string[] { "test@test.com" });
+        public MonthlyReportJob()
+        {
+
+        }
+        public Task Execute(IJobExecutionContext context)
+        {
+            object o = new object();
+            es.SendIt(receiverlist, o);
+            return Task.CompletedTask;
+        }
+
+
+    }
+}
---
+++ FinalAssessment.API/Quartz/WeeklyReportJob.cs
@@ -0,0 +1,22 @@
+﻿using FinalAssessment.Shared;
+using Quartz;
+
+namespace FinalAssessment.API.Quartz
+{
+    public class WeeklyReportJob : IJob
+    {
+        EmailSender es = new EmailSender();
+        List<string> receiverlist = new List<string>(new string[] { "dogukandiragan@hotmail.com" });
+        public WeeklyReportJob()
+        {
+
+        }
+        public Task Execute(IJobExecutionContext context)
+        {
+            object o = new object();
+            es.SendIt(receiverlist, o);
+            return Task.CompletedTask;
+        }
+
+    }
+}
